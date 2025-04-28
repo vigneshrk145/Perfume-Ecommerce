661e88d1784d404e0e85da5eb5f2216bec4f9e72
vignesh <vignesh032rk@gmail.com>
2025-04-28 16:18:24 +0530

    navbar changes

--- my-app/src/navbar.tsx
+++ my-app/src/navbar.tsx
@@ -1,10 +1,4 @@
-import Image from "next/image";
 import React from "react";
-import { BsTelephoneFill } from "react-icons/bs";
-import { FaLinkedin, FaTwitter } from "react-icons/fa";
-import { FaSquareInstagram } from "react-icons/fa6";
-import { IoMdMail } from "react-icons/io";
-import { RiFacebookFill } from "react-icons/ri";
 
 function Navbar() {
   return (
@@ -13,7 +7,7 @@ function Navbar() {
         <h4>FREE SHIPPING ON ALL ORDERS | ECO-FRIENDLY PACKAGING</h4>
       </div>
 
-      <div className="text-center py-4 font-bold tracking-tight bg-[black] text-[white] ">
+      <div className="text-center  py-4 font-bold tracking-tight bg-[black] text-[white] ">
         <h4>E S SENSORIE</h4>
       </div>
     </div>
